690f80c449ea03384a5e91c6b2dbb5f438df2b8c
kavyabhogiredy <83647748+kavyabhogiredy@users.noreply.github.com>
2021-05-12 20:44:22 +0530

    Add files via upload

---
+++ MouseListener.java
@@ -0,0 +1,15 @@
+MyClass implements MouseListener {
+    ...
+	someObject.addMouseListener(this);
+    ...
+    public void mousePressed(MouseEvent e) {
+    }
+    public void mouseReleased(MouseEvent e) {
+    }
+    public void mouseEntered(MouseEvent e) {
+    }
+    public void mouseExited(MouseEvent e) {
+    }
+    public void mouseClicked(MouseEvent e) {
+    }
+}
